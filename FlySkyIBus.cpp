9fb5d7e4f961e50be26a104cfa02f63b629a4c63
fr0zn <ferrancelades@hotmail.es>
2018-03-04 21:58:05 +0100

    Working write

--- FlySkyIBus.cpp
+++ FlySkyIBus.cpp
@@ -5,6 +5,8 @@
 #include <Arduino.h>
 #include "FlySkyIBus.h"
 
+#define DEBUG 1
+
 FlySkyIBus IBus;
 
 void FlySkyIBus::begin(HardwareSerial& serial)
@@ -24,26 +26,95 @@ void FlySkyIBus::begin(Stream& stream)
   this->lchksum = 0;
 }
 
+void FlySkyIBus::writeChannel(uint8_t channelNr, uint16_t value){
+
+  uint32_t now = millis();
+  uint16_t chksum = 0xff9f; // Len and cmd substracted
+  uint8_t tmp;
+
+  //stream->println(now-last);
+
+  if (now - last >= PROTOCOL_TIME)
+  {
+    state = WRITE_STATE;
+    last = now;
+  }
+
+  if (state == WRITE_STATE)
+  {
+
+    wbuffer[0] = 0x20; // LEN
+    wbuffer[1] = 0x40; // CMD
+
+    for (int i = 2; i < PROTOCOL_LENGTH - 2; i += 2 ) // All channels fill
+    {
+      tmp = LOW_VALUE & 0xff;
+      wbuffer[i] = tmp;
+      if (i != channelNr * 2)
+      {
+        chksum -= tmp;
+      }
+
+      tmp = LOW_VALUE >> 8;
+      wbuffer[i+1] = tmp;
+      if (i != channelNr * 2)
+      {
+        chksum -= tmp;
+      }
+    }
+
+    tmp = value & 0xff;
+    wbuffer[channelNr*2] = tmp;
+    chksum -= tmp;
+
+    tmp = value >> 8;
+    wbuffer[channelNr*2+1] = tmp;
+    chksum -= tmp;
+
+    wbuffer[PROTOCOL_LENGTH-2] = chksum & 0xff;
+    wbuffer[PROTOCOL_LENGTH-1] = chksum >> 8;
+
+    // for (int i = 0; i < PROTOCOL_LENGTH; i++)
+    // {
+    //   char b[16];
+    //   sprintf(b, "%x ", wbuffer[i]);
+    //   stream->print(b);
+    // }
+    // stream->println();
+
+    stream->write(wbuffer, PROTOCOL_LENGTH);
+
+  }
+
+}
+
 void FlySkyIBus::loop(void)
 {
+  // If serial data
   while (stream->available() > 0)
   {
     uint32_t now = millis();
+
+    // restart state, after 7/2 ms
     if (now - last >= PROTOCOL_TIMEGAP)
     {
       state = GET_LENGTH;
     }
+
     last = now;
     
-    uint8_t v = stream->read();
+    uint8_t value = stream->read(); // Read serial
+
+    // State machine
     switch (state)
     {
       case GET_LENGTH:
-        if (v <= PROTOCOL_LENGTH)
+
+        if (value <= PROTOCOL_LENGTH)
         {
           ptr = 0;
-          len = v - PROTOCOL_OVERHEAD;
-          chksum = 0xFFFF - v;
+          len = value - PROTOCOL_OVERHEAD;
+          chksum = 0xFFFF - value;
           state = GET_DATA;
         }
         else
@@ -53,8 +124,16 @@ void FlySkyIBus::loop(void)
         break;
 
       case GET_DATA:
-        buffer[ptr++] = v;
-        chksum -= v;
+
+        if (DEBUG)
+        {
+          char b[16];
+          sprintf(b, "DTA: 0x%x", value);
+          stream->println(b);
+        }
+
+        buffer[ptr++] = value;
+        chksum -= value;
         if (ptr == len)
         {
           state = GET_CHKSUML;
@@ -62,13 +141,15 @@ void FlySkyIBus::loop(void)
         break;
         
       case GET_CHKSUML:
-        lchksum = v;
+
+        lchksum = value;
         state = GET_CHKSUMH;
         break;
 
       case GET_CHKSUMH:
+
         // Validate checksum
-        if (chksum == (v << 8) + lchksum)
+        if (chksum == (value << 8) + lchksum)
         {
           // Execute command - we only know command 0x40
           switch (buffer[0])
@@ -77,6 +158,7 @@ void FlySkyIBus::loop(void)
               // Valid - extract channel data
               for (uint8_t i = 1; i < PROTOCOL_CHANNELS * 2 + 1; i += 2)
               {
+                // Write 16 bits (1000 to 2000 range)
                 channel[i / 2] = buffer[i] | (buffer[i + 1] << 8);
               }
               break;
--- FlySkyIBus.h
+++ FlySkyIBus.h
@@ -14,8 +14,10 @@ public:
   void begin(Stream& stream);
   void loop(void);
   uint16_t readChannel(uint8_t channelNr);
+  void writeChannel(uint8_t channelNr, uint16_t value);
 
 private:
+  // Used as a state machine
   enum State
   {
     GET_LENGTH,
@@ -23,23 +25,34 @@ private:
     GET_CHKSUML,
     GET_CHKSUMH,
     DISCARD,
+    WRITE_STATE,
   };
 
-  static const uint8_t PROTOCOL_LENGTH = 0x20;
-  static const uint8_t PROTOCOL_OVERHEAD = 3; // <len><cmd><data....><chkl><chkh>
-  static const uint8_t PROTOCOL_TIMEGAP = 3; // Packets are received very ~7ms so use ~half that for the gap
   static const uint8_t PROTOCOL_CHANNELS = 10;
+
+  static const uint8_t PROTOCOL_LENGTH = 0x20; // 32 bytes
+  static const uint8_t PROTOCOL_OVERHEAD = 3; // <len><cmd><data....><chkl><chkh>
+  static const uint8_t PROTOCOL_TIME = 7; // Packets are received very ~7ms
+  static const uint8_t PROTOCOL_TIMEGAP = 3; // Packets are received very ~7ms so use ~half that for the gap
   static const uint8_t PROTOCOL_COMMAND40 = 0x40; // Command is always 0x40
 
-  uint8_t state;
+  static const uint16_t LOW_VALUE = 0x3E8;
+  static const uint16_t HIGH_VALUE = 0x7D0;
+  static const uint16_t HALF_VALUE = 0x5DC;
+
+
   Stream* stream;
+  uint8_t state;
   uint32_t last;
-  uint8_t buffer[PROTOCOL_LENGTH];
   uint8_t ptr;
   uint8_t len;
-  uint16_t channel[PROTOCOL_CHANNELS];
+  uint8_t buffer[PROTOCOL_LENGTH];
+  uint8_t wbuffer[PROTOCOL_LENGTH+1];
   uint16_t chksum;
   uint8_t lchksum;
+  
+  uint16_t channel[PROTOCOL_CHANNELS];
+  
 };
 
 extern FlySkyIBus IBus;
